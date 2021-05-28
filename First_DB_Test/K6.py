fdb3dc1170027f0200712caa5537150562b2b440
Keytz <Keytz@mail.ru>
2021-05-29 00:01:17 +0300

    + вывод в csv

--- First_DB_Test/K6.py
+++ First_DB_Test/K6.py
@@ -5,25 +5,12 @@ conn = sqlite3.connect(r"k6_DB.db")
 cur=conn.cursor()
 cur.execute("select * from k6;")
 
+names = [description[0] for description in cur.description]
 all_result=cur.fetchall()
-# print(all_result)
-print("<elements>")
+f = open('output.txt', 'w')
+f.write("\t".join(names))
+f.write("\n")
 for row in all_result:
-    print("""    <element>
-        <id>{}</id>
-        <date>{}</date>
-        <page>{}</page>
-        <mode>{}</mode>
-        <req_total>{}</req_total>
-        <rec_min>{}</rec_min>
-        <rec_avg>{}</rec_avg>
-        <rec_max>{}</rec_max>
-        <req_0_220>{}</req_0_220>
-        <req_220_400>{}</req_220_400>
-        <req_400_600>{}</req_400_600>
-        <req_600_800>{}</req_600_800>
-        <rec_800_1000>{}</rec_800_1000>
-        <req_1000>{}</req_1000>
-    </element>""".format(row[0], row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13] )
-          )
-print("</elements>")
+    f.write("\t".join([str(x) for x in row]))
+    f.write("\n")
+f.close()
